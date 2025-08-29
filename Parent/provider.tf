0d18f91adcea6603e02dff344d2873bdebcb841b
Jitu <jitenderyadav86@gmail.com>
2025-08-29 22:19:20 +0530

    add

--- Parent/provider.tf
+++ Parent/provider.tf
@@ -1,13 +1,22 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      source = "hashicorp/azurerm"
+      version = "4.42.0"
     }
+  }
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "Donotdelete"
+    storage_account_name = "shivajitustorage1"
+    container_name       = "shivacontainer1"
+    key                  = "shiva.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "2f58bc69-5c25-4e57-96df-4f99e2da2be7"
-}
+  subscription_id ="2f58bc69-5c25-4e57-96df-4f99e2da2be7"
+  }
